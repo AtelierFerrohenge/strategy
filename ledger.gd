191f4cee07723949b47f46eec8d73b7a7c6875b4
Atelier Ferrohenge <kai@ferrohenge.com>
2025-11-25 16:39:43 -0800

    Unit addition functions

--- ledger.gd
+++ ledger.gd
@@ -1,4 +1,16 @@
+@abstract
 class_name Ledger
 extends Node
 
-@export var _units: Dictionary[Node, StringName]
+@export var map: Node = null
+@export var _units: Dictionary[Node, Variant] = {}
+
+
+func add_unit(unit: Node, location: Variant):
+	assert(not _units.has(unit), "The unit is already in the ledger.")
+	map.add_child(unit)
+	_place_unit(unit, location)
+	_units[unit] = location
+
+
+@abstract func _place_unit(unit: Node, location: Variant)
